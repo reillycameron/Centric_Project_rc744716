94cc5bb00f8aaa3d5ff279cdf0f830e09b89168c
Reilly Cameron <rc744716@ohio.edu>
2021-03-19 11:32:28 -0400

    Nominate Page Changes

--- Models/Nominate.cs
+++ Models/Nominate.cs
@@ -10,23 +10,21 @@ namespace Centric_Project_rc744716.Models
     {
         public int nominateID { get; set; }
 
-        [Required(Errormessage = "You must select an employee to nominate.")]
+        [Required(ErrorMessage = "You must select an employee to nominate.")]
 
         public int employeeID { get; set; }
         public virtual Profile profile { get; set; }
 
         public DateTime date { get; set; }
 
-        [Required(Errormessage = "You must select a Centric Value the employee has shown.")]
+        [Required(ErrorMessage = "You must select a Centric Value the employee has shown.")]
 
         public string valueRec { get; set; }
 
-        [Required(Errormessage = "You must add details on how the employee has shown the value.")]
+        [Required(ErrorMessage = "You must add details on how the employee has shown the value.")]
         [StringLength(250, ErrorMessage = "Please include details in 250 characters or less.")]
 
         public string valueComment { get; set; }
-
-        public ICollection<Profile> profile { get; set; }
 
     }
 }
