7832506d8a747e7f054c9361936e1643ba6afa65
Emma Hall <eh846316@ohio.edu>
2021-03-19 11:05:52 -0400

    Commit changes to Models

---
+++ Models/Nominate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Centric_Project_rc744716.Models
+{
+    public class Nominate
+    {
+        public int nominateID { get; set; }
+
+        [Required(Errormessage = "You must select an employee to nominate.")]
+
+        public int employeeID { get; set; }
+        public virtual Profile profile { get; set; }
+
+        public DateTime date { get; set; }
+
+        [Required(Errormessage = "You must select a Centric Value the employee has shown.")]
+
+        public string valueRec { get; set; }
+
+        [Required(Errormessage = "You must add a detail on how the employee has shown the value.")]
+        [StringLength(250, ErrorMessage = "Please include details in 250 characters or less.")]
+
+        public string valueComment { get; set; }
+
+        public ICollection<Profile> profile { get; set; }
+
+    }
+}
